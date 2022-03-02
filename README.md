53f8065b2ed968d78f9def1e48288fe0c49a8780
Raj Kumar <rajkumar@Raj-Web-Soft.local>
2022-03-02 21:32:05 +0530

    Demo work with SSR

---
+++ README.md
@@ -0,0 +1,5 @@
+Demo of server-side rendering using Laravel and Vue.js 2.5.
+
+To see an example including Vue Router, check out [this repo](https://github.com/rajwebsoft/vue-laravel).
+
+Requires the [php-v8js](https://github.com/phpv8/v8js) extension to be installed in your environment.
---
+++ app/Http/Controllers/AppController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\File;
+
+class AppController extends Controller
+{
+    private function render() {
+        
+        $renderer_source = File::get(base_path('node_modules/vue-server-renderer/basic.js'));
+        $app_source = File::get(public_path('js/entry-server.js'));
+        $vueSource = file_get_contents('https://cdnjs.cloudflare.com/ajax/libs/vue/2.0.3/vue.js');
+        $v8 = new V8Js();
+
+        ob_start();
+
+        $js =
+<<<EOT
+var process = { env: { VUE_ENV: "server", NODE_ENV: "production" } }; 
+this.global = { process: process };
+EOT;
+
+        $v8->executeString($js);
+        $v8->executeString($renderer_source);
+        $v8->executeString($app_source);
+
+        return ob_get_clean();
+    }
+
+    public function get() {
+        $ssr = $this->render();
+        return view('app', ['ssr' => $ssr]);
+    }
+}
